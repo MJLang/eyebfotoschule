40e32e6c7a3958f9fa5e1bcb45dbec34487c5892
Martin Lang <martin@mercator.io>
2013-06-27 17:29:31 -0700

    Completed with no bugs...

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,9 +16,25 @@ set :rails_env, 'production'
 set :scm, "git"
 set :repository, "git@github.com:MJLang/eyebfotoschule.git"
 set :branch, "master"
-
+set :normalize_asset_timestamps, false
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup"
+
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
+
+before "deploy:finalize_update", "deploy:assets:precompile"
