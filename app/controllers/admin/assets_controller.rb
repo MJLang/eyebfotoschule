a46f666a068cf24715270bdf1cb36bc3dfe764d6
Martin Lang <martin@mercator.io>
2013-06-29 13:23:01 -0700

    upload images

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -1,15 +1,30 @@
 class Admin::AssetsController < Admin::NestedController
-
+  layout 'admin'
   def index
-    @images = @parent.images
+    @assets = @parent.images
   end
 
   def create
-
+    @asset = @parent.images.new(asset_params)
+    if @asset.save
+      flash[:notice] = "Bild hochgeladen"
+      redirect_to admin_course_assets_path(@parent)
+    else
+      flash[:error] = "Da ging was schief"
+      redirect_to admin_course_assets_path(@parent)
+    end
   end
 
   def delete
+    @asset = Asset.find(params[:id])
+    @asset.delete
+    redirect_to admin_course_assets_path(@parent)
   end
 
+  private
+
+    def asset_params
+      params.require(:asset).permit!
+    end
 
 end
